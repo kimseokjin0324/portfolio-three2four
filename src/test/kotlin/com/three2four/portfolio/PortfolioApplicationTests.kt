3bfa78f74766bf958f83107ce7cede1ae39f4240
kimseokjin0324 <kimseokjin0324@gmail.com>
2025-12-16 01:28:16 +0900

    Git과 Github

---
+++ src/test/kotlin/com/three2four/portfolio/PortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.three2four.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
