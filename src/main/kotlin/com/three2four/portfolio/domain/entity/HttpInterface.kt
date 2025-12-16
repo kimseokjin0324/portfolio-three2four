95d4aaef870c51e6ac12ea8453569cf567eacd97
kimseokjin0324 <kimseokjin0324@gmail.com>
2025-12-16 23:47:21 +0900

    엔티티 개발 중간관계없음

--- src/main/kotlin/com/three2four/portfolio/domain/entity/HttpInterface.kt
+++ src/main/kotlin/com/three2four/portfolio/domain/entity/HttpInterface.kt
@@ -5,12 +5,29 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.servlet.http.HttpServletRequest
 
 @Entity
-class HttpInterface : BaseEntity() {
+class HttpInterface(httpServletRequest: HttpServletRequest) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "http_interface_id")
     var id: Long? = null
+
+    var cookies: String? = httpServletRequest.cookies?.map {
+        "${it.name}:${it.value}"
+    }?.toString()
+
+    var referer: String? = httpServletRequest.getHeader("referer")  //요청이 어디서부터왔냐 ex)구글에서 검색하면 구글임
+
+    var localAddr: String? = httpServletRequest.localAddr
+
+    var remoteAddr: String? = httpServletRequest.remoteAddr
+
+    var hostAddr: String? = httpServletRequest.remoteHost
+
+    var requestUri: String? = httpServletRequest.requestURI
+
+    var userAgent: String = httpServletRequest.getHeader("user-agent")
 
 }
--- src/main/kotlin/com/three2four/portfolio/domain/entity/Link.kt
+++ src/main/kotlin/com/three2four/portfolio/domain/entity/Link.kt
@@ -1,17 +1,24 @@
 package com.three2four.portfolio.domain.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
-class Link : BaseEntity() {
+class Link(
+    name:String,
+    content:String,
+    isActive:Boolean
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "link_id")
     var id: Long? = null
+    
+    var name: String = name
+    
+    var content: String = content
+
+    var isActive: Boolean = isActive
+    
 
 }
