1aea7db72468d1b364013287fec9f9da2633aa9b
kimseokjin0324 <kimseokjin0324@gmail.com>
2025-12-20 22:22:40 +0900

    인터셉터 개발

--- src/main/kotlin/com/three2four/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/three2four/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,8 +1,20 @@
 package com.three2four.portfolio.presentation.interceptor
 
+import com.three2four.portfolio.domain.entity.HttpInterface
+import com.three2four.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 
 @Component
-class PresentationInterceptor {
+class PresentationInterceptor(
+        private val httpInterfaceRepository: HttpInterfaceRepository
+):HandlerInterceptor{
 
+    override fun afterCompletion(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 }
